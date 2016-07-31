8014eec2fffa67f1912386ca14b93c5f3fbc72ce
Nick Belzer <nickbelzer@gmail.com>
2016-07-31 20:15:53 +0200

    Implement stats page

--- routes/stats.js
+++ routes/stats.js
@@ -1,11 +1,20 @@
 var express = require('express');
 var router = express.Router();
+var exec = require('child_process').exec;
 var spawn = require('child_process').spawn;
 
 var updating = false;
+var lastUpdate = new Date();
 
 router.get('/', function(req,res,next) {
-  res.send("Status page");
+  var uptime, nodeVersion, npmInfo;
+  exec('uptime', function(error, stdout, stderr) {
+    uptime = stdout;
+     exec('node -v', function(error, stdout, stderr) {
+      nodeVersion = stdout;
+      res.render('stats', { title: 'MateyByrd.Net | Status', uptime: uptime, lastUpdate: lastUpdate, nodeVersion: nodeVersion });
+     });
+  });
 });
 
 router.get('/update', function(req, res, next) {
