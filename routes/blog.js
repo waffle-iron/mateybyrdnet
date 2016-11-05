a7f2664902c04ed712747884af6989b326606301
Nick Belzer <nickbelzer@Nicks-MacBook-Air.local>
2016-11-05 13:24:11 +0100

    Remove previous files

--- routes/blog.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-router.get('/', function(req,res,next) {
-	res.render('blog', { title: 'Mateybyrd.Net | Dev blog' });
-});
-
-module.exports = router;
