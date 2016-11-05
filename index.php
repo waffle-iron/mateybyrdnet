42483a5ecec6584c3cecf47cfaf8249a027eede7
Nick Belzer <nickbelzer@gmail.com>
2016-11-05 15:56:49 +0100

    Minor changes to homepage

--- index.php
+++ index.php
@@ -4,4 +4,12 @@
  * Date: 31/10/2016
  */
 ?>
-Hello there, a website is under construction here.
+Hello there, a website is under construction here. <br><br>
+
+This website will contain a few functions that are currently being worked on. <br>
+<ul>
+    <li>Auto update system</li>
+    <li>Information about me ;)</li>
+    <li>Nice layout</li>
+</ul>
+
