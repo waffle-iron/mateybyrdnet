931f5803aad971106e2a7785bd4e5ce765b4782a
MateyByrd <nickbelzer@gmail.com>
2016-11-06 16:45:36 +0100

    Implement headerBuilder class
    For now a basic version has been implemented but this class will later on be able to provide a lot more flexibility in header information for each page.
    
    Currently it just creates a header from the given parameters (that is a title), but future plans could be different stylesheets per page and more general versitility.

--- includes/header.php
+++ includes/header.php
@@ -1,8 +1,17 @@
 <?php
 /**
  * Created by Nick Belzer
- * Date: 05/11/2016
+ * Date: 06/11/2016
  *
- * This file contains the basic header information that should be present on each page.
+ * This file contains the layout for the header, this file is used by the
+ * headerBuilder class and only this class should use this file. By calling
+ * include from that class we get to use the parameters of that class and
+ * thus insert the data that was given to the headerBuilder in the header.
  */
 ?>
+<head>
+    <title><?php echo $this->title; ?></title>
+    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+
+    <link rel="stylesheet" href="/includes/styling/main.css">
+</head>
