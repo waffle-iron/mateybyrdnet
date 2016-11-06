30e516a76d53183d071b5243aec010dd54f9d409
MateyByrd <nickbelzer@gmail.com>
2016-11-06 21:27:52 +0100

    Fix meta tag in header.php

--- includes/header.php
+++ includes/header.php
@@ -11,7 +11,7 @@
 ?>
 <head>
     <title><?php echo $this->title; ?></title>
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1\">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <link rel="stylesheet" href="/includes/styling/main.css">
     <script src="https://use.fontawesome.com/ba5e469969.js"></script>
