d38c1ca8e356716cbd60cd37ee6ad696bf83feb0
MateyByrd <nickbelzer@gmail.com>
2016-11-06 16:51:12 +0100

    Update index.php
    Let index.php now include the new headerBuilder. Improved html layout to prepare for styling.

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
+<html>
 <?php
 /**
  * Created by Nick Belzer.
  * Date: 31/10/2016
  */
+
+include "includes/headerBuilder.php";
+
+$header = new headerBuilder("MateyByrd.Net");
+$header->buildHeader();
+
 ?>
-Hello there, a website is under construction here. <br><br>
 
-This website will contain a few functions that are currently being worked on. <br>
-<ul>
-    <li>Auto update system</li>
-    <li>Information about me ;)</li>
-    <li>Nice layout</li>
-</ul>
+<body>
 
+    <p>
+        Hello there, a website is under construction here.
+    </p>
+
+    <p>
+        This website will contain a few functions that are currently being worked on.
+        <ul>
+            <li>Auto update system (works now!)</li>
+            <li>Information about me ;)</li>
+            <li>Nice layout</li>
+        </ul>
+    </p>
+
+</body>
+</html>
