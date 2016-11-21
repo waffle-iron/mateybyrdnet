05b8006463728f5670d1d0f68f32a70f6fd4577b
MateyByrd <nickbelzer@gmail.com>
2016-11-21 09:08:42 +0100

    Returning to NodeJS

--- status.php
+++
@@ -1,6 +0,0 @@
-<?php
-/**
- * Created by Nick Belzer.
- * Date: 31/10/2016
- */
-?>
--- updater.php
+++
@@ -1,9 +0,0 @@
-<?php
-/**
- * Created by Nick Belzer
- * Date: 05/11/2016
- *
- * File that updates the entire website using a shell_script.
- */
-
-echo shell_exec("sh updateDev.sh");
