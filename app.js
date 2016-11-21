a781b4e6383705be774d236c150fe6a2d226a144
MateyByrd <nickbelzer@gmail.com>
2016-11-21 09:40:29 +0100

    Implement sass in Node

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var sassMiddleware = require('node-sass-middleware');
 var path = require('path');
 var favicon = require('serve-favicon');
 var logger = require('morgan');
@@ -19,8 +20,15 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+    sassMiddleware({
+        src: path.join(__dirname, '/public/sass'),
+        dest: path.join(__dirname, '/public/stylesheets'),
+        debug: true,
+    })
+);
 
+app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', index);
 
 // catch 404 and forward to error handler
