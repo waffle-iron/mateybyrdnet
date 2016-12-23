b485c66320975dac8fb290de51e7e323d02cfd14
MateyByrd <nickbelzer@gmail.com>
2016-12-24 00:38:00 +0100

    Update miningClient js & Add load button on index

--- public/javascript/miningGameClient.js
+++ public/javascript/miningGameClient.js
@@ -2,30 +2,62 @@
  * Created by nickbelzer on 23/12/2016.
  */
 
-function main() {
-  $('#mining-game').load('/mining');
+/**
+ * MiningGame Object, stores all data that has to do with the game.
+ */
+var MiningGame = function() {
 
-  $.get('/mining/checkSession', function() {
+  /**
+   * Function that starts the game, it does this by loading the game in to
+   * view and downloading all the session data from the server.
+   */
+  this.startGame = function() {
+    $('#mining-game').load('/mining');
+
+    $.get('/mining/checkSession', function() {
+      $.getJSON('/mining/sessionData', updatePersonal);
+      holdConnection();
+    });
+
+    // Make sure we let the server know we are still connected.
+    setInterval(holdConnection, 5000);
+  };
+
+  /**
+   * Method used to let the server know we are still conencted.
+   */
+  function holdConnection() {
+    $.getJSON('/mining/connected', updateTotalCount);
+  };
+
+  /**
+   * Updates the total count of resources mined.
+   *
+   * @param data The data json that contains the total amount of crystals mined.
+   */
+  function updateTotalCount(data) {
+    $('#totalCrystals').text(data['crystals']);
+
+    // Because updateTotalCount is called each 5 seconds it is a good moment
+    // to also update the personal data of this session.
     $.getJSON('/mining/sessionData', updatePersonal);
-  });
+  };
 
-  holdConnection();
-  setInterval(holdConnection, 5000);
+  /**
+   * Updates the personal data of the session like amount of workers and
+   * amount of crystals mined.
+   *
+   * @param data The data json that contains the session specific data.
+   */
+  function updatePersonal(data) {
+    $('#personalWorkers').text(data['workers']);
+    $('#personalCrystals').text(data['crystals']);
+  };
+};
+
+function loadGame() {
+  var miningGame = new MiningGame();
+  miningGame.startGame();
 }
 
-function holdConnection() {
-  $.getJSON('/mining/connected', updateTotalCount);
-}
-
-function updateTotalCount(data) {
-  $('#totalCrystals').text(data['crystals']);
-
-  $.getJSON('/mining/sessionData', updatePersonal);
-}
-
-function updatePersonal(data) {
-  $('#personalWorkers').text(data['workers']);
-  $('#personalCrystals').text(data['crystals']);
-}
-
-$(document).ready(main);
+$(document).ready();
