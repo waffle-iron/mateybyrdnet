a1dfa4367e0bfe20812ba7ba0c92e89f1ca86e60
Nick Belzer <nickbelzer@gmail.com>
2016-12-25 01:48:12 +0100

    Add initial christmas page

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,8 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MateyByrd.Net' });
 });
 
+router.get('/christmas', function(req, res, next) {
+  res.render('christmas', { title: 'Merry Christmas' });
+});
+
 module.exports = router;
