e9ba02cf44fdbe5df6591973f0707608cf85a844
Nick Belzer <nickbelzer@gmail.com>
2016-12-25 02:37:40 +0100

    Update christmas page

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,24 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'MateyByrd.Net' });
 });
 
+
 router.get('/christmas', function(req, res, next) {
-  res.render('christmas', { title: 'Merry Christmas' });
+  res.render('christmas', { title: 'Merry Christmas', sender: 'Nick Belzer', receiver: '' });
 });
+
+router.get('/christmas/:receiver', function(req, res, next) {
+  var receiver = req.params.receiver;
+  if (!receiver) receiver='';
+  res.render('christmas', { title: 'Merry Christmas', receiver: receiver, sender: 'Nick Belzer' });
+});
+
+router.get('/christmas/:receiver/:sender', function(req, res, next) {
+  var receiver = req.params.receiver;
+  if (!receiver) receiver='';
+  var sender = req.params.sender;
+  if (!receiver) sender='Nick Belzer';
+  res.render('christmas', { title: 'Merry Christmas', receiver: receiver, sender: sender });
+});
+
 
 module.exports = router;
