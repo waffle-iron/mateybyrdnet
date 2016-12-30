c3b0ce47d5ed273a83f54a92c55eb72f2a253c8e
Nick Belzer <nickbelzer@gmail.com>
2016-12-30 22:41:21 +0100

    Add resend possibility

--- routes/newYear.js
+++ routes/newYear.js
@@ -16,24 +16,27 @@ router.get('/:receiver', renderNewYear);
 
 router.get('/:receiver/:sender', renderNewYear);
 
+router.get('/:receiver/:sender/:message', renderNewYear);
+
 function renderNewYear(req, res) {
   var receiver = req.params.receiver;
-  if (!receiver) receiver='';
+  if (!receiver) receiver='To You';
   var sender = req.params.sender;
   if (!sender) sender='Nick Belzer';
+  var message = req.params.message;
+  if (!message) message=getMessageByCode('default').message;
 
-  res.render('newYear', { title: 'Happy New Year', sender: sender, receiver: receiver, message: getMessageByCode('default').message});
+  res.render('newYear', {title: 'Happy New Year', sender: sender, receiver: receiver, message: message});
 }
 
 function renderNewYearByCode(req, res) {
   var message;
   message = getMessageByCode(req.params.code);
-  res.render('newYear', { title: 'Happy New Year', sender: 'Nick Belzer', receiver: message.for, message: message.message});
+  res.render('newYear', {title: 'Happy New Year', sender: 'Nick Belzer', receiver: message.for, message: message.message});
 }
 
 function getMessageByCode(code) {
   for (var i = 0; i < messages.length; i++) {
-    console.log(messages[i].code);
     if (code == messages[i].code)
       return messages[i];
   }
